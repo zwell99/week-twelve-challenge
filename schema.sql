62cbc82c4e09bca1142c29621208e22dee404635
Zach Wellens <zachwellens990@gmail.com>
2023-06-20 14:31:31 -0400

    Created database tables

--- schema.sql
+++ schema.sql
@@ -1,3 +1,29 @@
 DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 USE company_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30)
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+)
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+)
